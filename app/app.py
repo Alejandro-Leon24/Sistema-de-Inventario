4ee34afac5274a30d31d333f08d0e689b1167062
Alejandro_Leon <alejandroxavier2004@gmail.com>
2026-04-07 15:17:27 -0500

    cambios en la importaciòn del proyecto y ejecuciòn

--- app/app.py
+++ app/app.py
@@ -3,8 +3,9 @@ import sys
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-if str(BASE_DIR) not in sys.path:
-    sys.path.append(str(BASE_DIR))
+if str(BASE_DIR) in sys.path:
+    sys.path.remove(str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
 from flask import Flask, render_template
 
